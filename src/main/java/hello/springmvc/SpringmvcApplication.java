a88b996ef80ef1be82c4190749103cba4b7b8474
Hoony <xognsdl710@naver.com>
2023-04-21 19:52:01 +0900

    <4. 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술> 6) '스프링 MVC 기본기능' 프로젝트 생성 - 최초 commit

--- src/main/java/hello/springmvc/SpringmvcApplication.java
+++ src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringmvcApplication {
+	// commit 확인
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmvcApplication.class, args);
